48a796db4bf2f0cd50ce3054d5e370c2daa8090f
Julio Yu <exjulio56@gmail.com>
2012-02-06 19:14:40 -0430

    Segundo commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Danta::Application.config.secret_token = 'a2f00084cd4c40fc9910dfa70b1ba40dc0af016b0d3d64a541eebe12baaa15cc7e92c5badeada541431921fc30cb1e385fc629df7ed74bfbdffc5a20ac9ff81c'
