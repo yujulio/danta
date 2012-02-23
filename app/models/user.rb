1c7819a3a72e8a67a64a9267f46c1c47b2b09376
Carlos Torrealba <hapdcore@gmail.com>
2012-02-23 12:00:43 -0430

    version 1.0 de las validaciones del sistema

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  validates :first_name, :last_name, :id_number, :gender, :presence => true
+  validates :email, :confirmation => true
+  validates :email_confirmation, :presence => true
+  validates :email, :uniqueness => true
+  validates :password, :confirmation => true, :length => { :in => 6..20 }
+  validates :password_confirmation, :presence => true
 end
